9d480a36d54ecce9d37eec990a91606d262b293a
Eddie He <hlhedison@gmail.com>
2023-02-23 22:11:00 +1300

    New Time Record has been added

--- Feb2023/Feb2023/Program.cs
+++ Feb2023/Feb2023/Program.cs
@@ -22,17 +22,63 @@ passwordTextbox.SendKeys("123123");
 IWebElement loginButton = driver.FindElement(By.XPath("//*[@id=\"loginForm\"]/form/div[3]/input[1]"));
 loginButton.Click();
 
-// Check if user has successfully logged in
-IWebElement loginMessage = driver.FindElement(By.XPath("//*[@id=\"logoutForm\"]/ul/li/a"));
+// Create a new time record
+// Navigate to Time and Material page
+IWebElement administrationDropDown = driver.FindElement(By.XPath("/html/body/div[3]/div/div/ul/li[5]/a"));
+administrationDropDown.Click();
 
-if(loginMessage.Text == "Hello hari!")
+IWebElement tmOption = driver.FindElement(By.XPath("/html/body/div[3]/div/div/ul/li[5]/ul/li[3]/a"));
+tmOption.Click();
+
+// Click create new button
+IWebElement createNewButton = driver.FindElement(By.XPath("//*[@id=\"container\"]/p/a"));
+createNewButton.Click();
+
+// Select Time option from TypeCode dropdown list
+IWebElement typeCode = driver.FindElement(By.XPath("//*[@id=\"TimeMaterialEditForm\"]/div/div[1]/div/span[1]/span"));
+typeCode.Click();
+
+IWebElement timeOption = driver.FindElement(By.XPath("//*[@id=\"TimeMaterialEditForm\"]/div/div[1]/div/span[1]/span/span[1]"));
+timeOption.Click();
+
+// Input code into code textbox
+IWebElement codeTextBox = driver.FindElement(By.Id("Code"));
+codeTextBox.SendKeys("Feb2023");
+
+// Input Description into description textbox
+IWebElement descriptionTextBox = driver.FindElement(By.Id("Description"));
+descriptionTextBox.SendKeys("Feb2023");
+
+// Input Prive per unit into price per unit textbox
+IWebElement priceTextbox = driver.FindElement(By.XPath("//*[@id=\"TimeMaterialEditForm\"]/div/div[4]/div/span[1]/span/input[1]"));
+priceTextbox.SendKeys("510");
+
+// Click on the save button
+IWebElement saveButton = driver.FindElement(By.Id("SaveButton"));
+saveButton.Click();
+
+// Wait for 5s
+Thread.Sleep(5000);
+
+// Check if time record has been created
+IWebElement goToLastPage = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[4]/a[4]/span"));
+goToLastPage.Click();
+
+// Wait for 5s
+Thread.Sleep(5000);
+
+// Navigate to the last one record
+IWebElement newTimeRecord = driver.FindElement(By.XPath("//*[@id=\"tmsGrid\"]/div[3]/table/tbody/tr[last()]/td[1]"));
+if (newTimeRecord.Text == "Feb2023")
 {
-    Console.WriteLine("Logged in successfully");
+    Console.WriteLine("New Time Record Created Sucessfully!!!");
 }
 else
 {
-    Console.WriteLine("Login failed");
+    Console.WriteLine("New Time Record Hasn't Been Created!!!");
 }
+// Wait for 5s
+Thread.Sleep(5000);
 
 // Close the browser
 driver.Quit();
